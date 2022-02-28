261dce196747e41faf8b7c2d7a0adc3d1fd48908
xinyu-g <xinyuccyy@gmail.com>
2022-02-27 23:12:52 -0600

    delete acc info

--- lambda_build.py
+++ lambda_build.py
@@ -1,16 +1,15 @@
 import boto3
 import logging
 import sys
-# 'start_ec2instance'
-# arn:aws:iam::108172868018:role/x_lambda
+
 
 def lambda_build(LambdaFunctionName, RoleName):
 
     client = boto3.client(
         'lambda', 
         region_name='us-east-1',
-        aws_access_key_id='AKIARSL427GZFQ2VV5X4',
-        aws_secret_access_key='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'
+        aws_access_key_id='',
+        aws_secret_access_key=''
     )
 
     create_lambda_function = client.create_function(
